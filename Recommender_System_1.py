338d543084fcd17df6bcabd06362cde16c8eed7c
Rakshit Bhardwaj <143745073+rbhardwaj2186@users.noreply.github.com>
2024-05-02 23:48:42 -0500

    Update Recommender_System_1.py

--- Recommender_System_1.py
+++ Recommender_System_1.py
@@ -17,3 +17,18 @@ ratings = tfds.load('movielens/100k-ratings', split='train')
 
 for x in ratings.take(1).as_numpy_iterator():
     pprint.pprint(x)
+
+# Features of all the available movies
+movies = tfds.load('movielens/100k-movies', split='train')
+
+for x in movies.take(1).as_numpy_iterator():
+    pprint(x).pprint(x)
+
+tf.random.set_seed(42)
+shuffled=ratings.shuffle(100_00, seed=42, reshuffle_each_iteration=False)
+
+train = shuffled.take(80_000)
+test = shuffled.skip(80_000).take(20_000)
+
+movie_titles = movie.batch(1_000)
+user_ids = ratings.batch(1_00_000).map(lambda x: x['user_id'])
