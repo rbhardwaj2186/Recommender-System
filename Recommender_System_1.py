55027d89472600a1bb4dac3071ba1e85fafa2c27
Rakshit Bhardwaj <143745073+rbhardwaj2186@users.noreply.github.com>
2024-05-02 23:58:02 -0500

    Update Recommender_System_1.py

--- Recommender_System_1.py
+++ Recommender_System_1.py
@@ -9,8 +9,9 @@ import matplotlib.pyplot as plt
 import tensorflow_datasets as tfds
 from scipy.stats import linregress
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import pprint
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # Ratings data
 ratings = tfds.load('movielens/100k-ratings', split='train')
@@ -25,15 +26,15 @@ for x in movies.take(1).as_numpy_iterator():
     pprint.pprint(x)
 
 tf.random.set_seed(42)
-shuffled=ratings.shuffle(100_00, seed=42, reshuffle_each_iteration=False)
+shuffled = ratings.shuffle(100_00, seed=42, reshuffle_each_iteration=False)
 
 train = shuffled.take(80_000)
 test = shuffled.skip(80_000).take(20_000)
 
 movie_titles = movies.batch(1_000)
-user_ids = ratings.batch(1_00_000).map(lambda x: x['user_id'])
+user_ids = ratings.batch(1_000_000).map(lambda x: x['user_id'])
 
 unique_movie_titles = np.unique(np.concatenate(list(movie_titles)))
 unique_user_ids = np.unique((np.concatenate(list(user_ids))))
 
-print(movie_titles)
+print(unique_movie_titles[:4])
