d03afbb4fefdcdd0c00b1afbc97fce1ae2518db2
Rakshit Bhardwaj <143745073+rbhardwaj2186@users.noreply.github.com>
2024-05-04 23:09:39 -0500

    Update Recommender_System_1.py

--- Recommender_System_1.py
+++ Recommender_System_1.py
@@ -19,22 +19,35 @@ ratings = tfds.load('movielens/100k-ratings', split='train')
 for x in ratings.take(1).as_numpy_iterator():
     pprint.pprint(x)
 
-# Features of all the available movies
 movies = tfds.load('movielens/100k-movies', split='train')
 
 for x in movies.take(1).as_numpy_iterator():
     pprint.pprint(x)
 
 tf.random.set_seed(42)
-shuffled = ratings.shuffle(100_00, seed=42, reshuffle_each_iteration=False)
+shuffled = ratings.shuffle(100_000, seed=42, reshuffle_each_iteration=False)
 
 train = shuffled.take(80_000)
 test = shuffled.skip(80_000).take(20_000)
 
-movie_titles = movies.batch(1_000)
-user_ids = ratings.batch(1_000_000).map(lambda x: x['user_id'])
+#movie_titles = movies.batch(1_000)
+#user_ids = ratings.batch(1_000_000).map(lambda x: x['user_id'])
 
-unique_movie_titles = np.unique(np.concatenate(list(movie_titles)))
-unique_user_ids = np.unique((np.concatenate(list(user_ids))))
+#unique_movie_titles = np.unique(np.concatenate(list(movie_titles)))
+##unique_movie_titles = np.unique(np.concatenate(list(movie_titles.flat_map(lambda x: x['movie_title']))))
+#unique_user_ids = np.unique((np.concatenate(list(user_ids))))
+# Flatten and extract movie titles
+movie_titles = movies.flat_map(lambda x: tf.data.Dataset.from_tensor_slices([x['movie_title']]))
+#unique_movie_titles = np.unique(np.concatenate(list(movie_titles.as_numpy_iterator())))
+unique_movie_titles = np.unique(np.array(list(movie_titles.as_numpy_iterator())))
+
+
+# Extract user IDs
+user_ids = ratings.map(lambda x: x['user_id'])
+#unique_user_ids = np.unique(np.concatenate(list(user_ids.as_numpy_iterator())))
+unique_user_ids = np.unique(np.array(list(user_ids.as_numpy_iterator())))
 
 print(unique_movie_titles[:4])
+
+
+
